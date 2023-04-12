7c94119009c6f9cb019ab0b3c2b64101a38146c4
codematsing <sam.solis@codematsing.com>
2023-04-12 16:07:38 +0800

    removed @ in admin.py

--- {%raw%}{{cookiecutter.app_name}}{%endraw%}/admin.py
+++ {%raw%}{{cookiecutter.app_name}}{%endraw%}/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from models import {{cookiecutter.snake_case_model_name}}
 
 # Register your models here.
-@admin.site.register({{cookiecutter.snake_case_model_name}})
+admin.site.register({{cookiecutter.snake_case_model_name}})
