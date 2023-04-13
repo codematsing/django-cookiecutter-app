0a809d796a80c74ab8a49fa27859aa0251cf27a1
codematsing <sam.solis@codematsing.com>
2023-04-13 18:27:53 +0800

    test_urls fixed

--- {%raw%}{{cookiecutter.app_name}}{%endraw%}/tests/test_urls.py
+++ {%raw%}{{cookiecutter.app_name}}{%endraw%}/tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.urls import resolve, reverse
 from django.test import TestCase
 from django.test import Client
-from {{ cookiecutter.project_slug }}.users.models import User
+from {{ cookiecutter.project_slug }}.users.tests.factories import UserFactory
 import logging
 logger = logging.getLogger(__name__)
 
